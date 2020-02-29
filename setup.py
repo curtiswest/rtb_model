8974a10f7f905ed3c453a96d1d99194000fd1d0f
Curtis West <curtis.west@vaxagroup.com>
2020-02-29 16:54:14 +1000

    Add psycopg2 dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='rtb_model',
-      version='1.0',
+      version='1.1',
       description='The data model backing Raise the BAR',
       url='git@github.com:curtiswest/rtb_model.git',
       author='Curtis West',
@@ -11,4 +11,4 @@ setup(name='rtb_model',
       license='None',
       packages=['rtb_model'],
       zip_safe=False,
-      install_requires=['sqlalchemy', 'python-decouple', 'pandas', 'pytz', 'requests'])
+      install_requires=['sqlalchemy', 'python-decouple', 'pandas', 'pytz', 'requests', 'psycopg2'])
